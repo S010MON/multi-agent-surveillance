09c130f88217718a724956745070a897c3f34f6d
S010MON <10490509+S010MON@users.noreply.github.com>
2022-02-03 21:04:06 +0100

    Vector class added

---
+++ src/main/java/app/controller/Vector.java
@@ -0,0 +1,44 @@
+package app.controller;
+
+public class Vector
+{
+    private double x;
+    private double y;
+
+    public Vector()
+    {
+        this.x = 0d;
+        this.y = 0d;
+    }
+
+    public Vector(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public Vector add(Vector other)
+    {
+        return new Vector(this.x + other.getX(), this.y + other.getY());
+    }
+
+    public Vector sub(Vector other)
+    {
+        return new Vector(this.x - other.getX(), this.y - other.getY());
+    }
+
+    public Vector scale(double scalar)
+    {
+        return new Vector(this.getX() * scalar, this.getY() * scalar);
+    }
+}
