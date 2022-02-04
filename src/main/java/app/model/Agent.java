073e49cd52342ff1877fb5102762b12d01ee0cc2
S010MON <10490509+S010MON@users.noreply.github.com>
2022-02-04 09:29:54 +0100

    Adds getView and getHearing to Agent

--- src/main/java/app/model/Agent.java
+++ src/main/java/app/model/Agent.java
@@ -1,10 +1,16 @@
 package app.model;
 
+import app.controller.Ray;
 import app.controller.Vector;
+import java.util.ArrayList;
 
 public interface Agent extends Placeable
 {
     void move();
 
     Vector getDirection();
+
+    ArrayList<Ray> getView();
+
+    double getHearing();
 }
