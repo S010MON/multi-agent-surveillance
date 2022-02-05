47753b0241a298210a83987f35048447ff69e6a6
MatthewFoster02 <mattfoster02@outlook.com>
2022-02-05 18:49:56 +0100

    Removed constructor.
    Constructor removed and setters with lock system added.

--- src/main/java/app/controller/Settings.java
+++ src/main/java/app/controller/Settings.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 
 public class Settings
 {
+    private boolean unlocked = true;
     private int gamemode;
     private int height;
     private int width;
     private int noOfGuards;
     private int noOfIntruders;
-    private double walkSpeed;
-    private double sprintSpeed;
+    private double walkSpeedGuard;
+    private double sprintSpeedGuard;
+    private double walkSpeedIntruder;
+    private double sprintSpeedIntruder;
     private double timeStep;
     private double scaling;
     private ArrayList<Rectangle2D> walls;
@@ -23,25 +26,153 @@ public class Settings
     private Rectangle2D.Double spawnAreaIntruders;
     private Rectangle2D.Double spawnAreaGuards;
 
-    public Settings(int gamemode, int height, int width, int noOfGuards, int noOfIntruders, double walkSpeed, double sprintSpeed, double timeStep, double scaling, Rectangle2D[] walls, Rectangle2D[] shade, Rectangle2D[] towers, Rectangle2D[] portals, Rectangle2D[] textures, Rectangle2D.Double targetArea, Rectangle2D.Double spawnAreaIntruders, Rectangle2D.Double spawnAreaGuards)
+    public void setGamemode(int gamemode)
     {
-        this.gamemode = gamemode;
-        this.height = height;
-        this.width = width;
-        this.noOfGuards = noOfGuards;
-        this.noOfIntruders = noOfIntruders;
-        this.walkSpeed = walkSpeed;
-        this.sprintSpeed = sprintSpeed;
-        this.timeStep = timeStep;
-        this.scaling = scaling;
-        this.walls = walls;
-        this.shade = shade;
-        this.towers = towers;
-        this.portals = portals;
-        this.textures = textures;
-        this.targetArea = targetArea;
-        this.spawnAreaIntruders = spawnAreaIntruders;
-        this.spawnAreaGuards = spawnAreaGuards;
+        if(unlocked)
+            this.gamemode = gamemode;
+    }
+
+    public void setHeight(int height)
+    {
+        if(unlocked)
+            this.height = height;
+    }
+
+    public void setWidth(int width)
+    {
+        if(unlocked)
+            this.width = width;
+    }
+
+    public void setNoOfGuards(int noOfGuards)
+    {
+        if(unlocked)
+            this.noOfGuards = noOfGuards;
+    }
+
+    public void setNoOfIntruders(int noOfIntruders)
+    {
+        if(unlocked)
+            this.noOfIntruders = noOfIntruders;
+    }
+
+    public void setWalkSpeedGuard(double walkSpeedGuard)
+    {
+        if(unlocked)
+            this.walkSpeedGuard = walkSpeedGuard;
+    }
+
+    public void setSprintSpeedGuard(double sprintSpeedGuard)
+    {
+        if(unlocked)
+            this.sprintSpeedGuard = sprintSpeedGuard;
+    }
+
+    public void setWalkSpeedIntruder(double walkSpeedIntruder)
+    {
+        if(unlocked)
+            this.walkSpeedIntruder = walkSpeedIntruder;
+    }
+
+    public void setSprintSpeedIntruder(double sprintSpeedIntruder)
+    {
+        if(unlocked)
+            this.sprintSpeedIntruder = sprintSpeedIntruder;
+    }
+
+    public void setTimeStep(double timeStep)
+    {
+        if(unlocked)
+            this.timeStep = timeStep;
+    }
+
+    public void setScaling(double scaling)
+    {
+        if(unlocked)
+            this.scaling = scaling;
+    }
+
+    public void setWalls()
+    {
+        if(unlocked)
+            this.walls = new ArrayList<>();
+    }
+
+    public void addWall(Rectangle2D wall)
+    {
+        if(unlocked)
+            this.walls.add(wall);
+    }
+
+    public void setShade()
+    {
+        if(unlocked)
+            this.shade = new ArrayList<>();
+    }
+
+    public void addShade(Rectangle2D shade)
+    {
+        if(unlocked)
+            this.walls.add(shade);
+    }
+
+    public void setTowers()
+    {
+        if(unlocked)
+            this.towers = new ArrayList<>();
+    }
+
+    public void addTower(Rectangle2D tower)
+    {
+        if(unlocked)
+            this.walls.add(tower);
+    }
+
+    public void setPortals()
+    {
+        if(unlocked)
+            this.portals = new ArrayList<>();
+    }
+
+    public void addPortal(Rectangle2D portal)
+    {
+        if(unlocked)
+            this.walls.add(portal);
+    }
+
+    public void setTextures()
+    {
+        if(unlocked)
+            this.textures = new ArrayList<>();
+    }
+
+    public void addTexture(Rectangle2D texture)
+    {
+        if(unlocked)
+            this.walls.add(texture);
+    }
+
+    public void setTargetArea(Rectangle2D.Double targetArea)
+    {
+        if(unlocked)
+            this.targetArea = targetArea;
+    }
+
+    public void setSpawnAreaIntruders(Rectangle2D.Double spawnAreaIntruders)
+    {
+        if(unlocked)
+            this.spawnAreaIntruders = spawnAreaIntruders;
+    }
+
+    public void setSpawnAreaGuards(Rectangle2D.Double spawnAreaGuards)
+    {
+        if(unlocked)
+            this.spawnAreaGuards = spawnAreaGuards;
+    }
+
+    public void lock()
+    {
+        this.unlocked = false;
     }
 
     public int getGamemode()
@@ -69,14 +200,24 @@ public class Settings
         return this.noOfIntruders;
     }
 
-    public double getWalkSpeed()
+    public double getWalkSpeedGuard()
     {
-        return this.walkSpeed;
+        return this.walkSpeedGuard;
     }
 
-    public double getSprintSpeed()
+    public double getSprintSpeedGuard()
     {
-        return this.sprintSpeed;
+        return this.sprintSpeedGuard;
+    }
+
+    public double getWalkSpeedIntruder()
+    {
+        return this.walkSpeedIntruder;
+    }
+
+    public double getSprintSpeedIntruder()
+    {
+        return this.sprintSpeedIntruder;
     }
 
     public double getTimeStep()
