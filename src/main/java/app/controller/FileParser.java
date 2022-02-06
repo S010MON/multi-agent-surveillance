84e58e51659a0a03cad2a3f2c235bede08c0dad5
MatthewFoster02 <mattfoster02@outlook.com>
2022-02-06 21:34:38 +0100

    Added switch case.
    Switch case added to parse each line of the file.

--- src/main/java/app/controller/FileParser.java
+++ src/main/java/app/controller/FileParser.java
@@ -1,5 +1,7 @@
 package app.controller;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.util.Scanner;
 
 public class FileParser
@@ -48,12 +50,64 @@ public class FileParser
                     case "width":
                         map.setWidth(Integer.parseInt(val));
                         break;
+                    case "numGuards":
+                        map.setNoOfGuards(Integer.parseInt(val));
+                        break;
+                    case "numIntruders":
+                        map.setNoOfIntruders(Integer.parseInt(val));
+                        break;
+                    case "baseSpeedGuard":
+                        map.setWalkSpeedGuard(Double.parseDouble(val));
+                        break;
+                    case "sprintSpeedGuard":
+                        map.setSprintSpeedGuard(Double.parseDouble(val));
+                        break;
+                    case "baseSpeedIntruder":
+                        map.setWalkSpeedIntruder(Double.parseDouble(val));
+                        break;
+                    case "sprintSpeedIntruder":
+                        map.setSprintSpeedIntruder(Double.parseDouble(val));
+                        break;
+                    case "timeStep":
+                        map.setTimeStep(Double.parseDouble(val));
+                        break;
+                    case "scaling":
+                        map.setScaling(Double.parseDouble(val));
+                        break;
+                    case "wall":
+                        map.addWall(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        break;
+                    case "shaded":
+                        map.addShade(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        break;
+                    case "tower":
+                        map.addTower(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        break;
+                    case "teleport":
+                        map.addPortal(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        map.addTeleportTo(new Point(Integer.parseInt(coords[4]), Integer.parseInt(coords[5])));
+                        // Here coords[6] is the orientation, no element in settings for this, can be added if needed
+                        break;
+                    case "texture":
+                        map.addTexture(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        map.addTextureType(Integer.parseInt(coords[4]));
+                        // Here coords[5] is the orientation, can also be added like above for portals
+                        break;
+                    case "targetArea":
+                        map.setTargetArea(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        break;
+                    case "spawnAreaIntruders":
+                        map.setSpawnAreaIntruders(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        break;
+                    case "spawnAreaGuards":
+                        map.setSpawnAreaGuards(new Rectangle2D.Double(Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Integer.parseInt(coords[2]) - Integer.parseInt(coords[0]), Integer.parseInt(coords[3]) - Integer.parseInt(coords[1])));
+                        break;
                 }
             }
         }
         catch(Exception e)
         {
-            System.out.println("Line with exception: "+countLines);
+            System.out.println("Line causing failure: "+countLines);
             System.out.println("Content of line: "+nextLine);
         }
     }
