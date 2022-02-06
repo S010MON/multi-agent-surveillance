13d5f29c09d507dd4a8bde179d87e7bd0bb0b5bd
Travis <travisd1711@gmail.com>
2022-02-06 15:55:59 +0100

    "Manual Agent class"

---
+++ src/main/java/app/controller/ManualAgent.java
@@ -0,0 +1,15 @@
+package app.controller;
+
+public class ManualAgent extends AgentImp
+{
+    public ManualAgent(Vector position, Vector direction, double radius)
+    {
+        super(position, direction, radius);
+    }
+
+    @Override
+    public void move()
+    {
+
+    }
+}
