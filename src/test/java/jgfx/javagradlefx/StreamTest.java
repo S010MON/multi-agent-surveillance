a7e403c010625658c93eafe252eb31588543e4f2
S010MON <10490509+S010MON@users.noreply.github.com>
2022-02-09 12:46:05 +0100

    implements functional method to raytrace

---
+++ src/test/java/jgfx/javagradlefx/StreamTest.java
@@ -0,0 +1,32 @@
+package jgfx.javagradlefx;
+
+import app.controller.linAlg.Vector;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StreamTest
+{
+    @Test void testMin()
+    {
+        ArrayList<Vector> V = new ArrayList<>();
+        Vector o = new Vector(0,0);
+        V.add(new Vector(10,10));
+        V.add(new Vector(7,10));
+        V.add(new Vector(10,7));
+        V.add(new Vector(3,3));
+        V.add(new Vector(10,11));
+
+        Vector min = V.stream()
+                .min(Comparator.comparing(e -> e.dist(o)))
+                .get();
+
+        assertEquals(3,min.getX());
+        assertEquals(3,min.getY());
+    }
+}
+
+
+
