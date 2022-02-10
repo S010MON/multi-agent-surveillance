2a1dc52da1ec225f9db7f17ad9cff81495ceb782
MatthewFoster02 <mattfoster02@outlook.com>
2022-02-10 19:39:20 +0100

    Update FileMenuBar.java
    Small change to label of menus

--- src/main/java/app/view/FileMenuBar.java
+++ src/main/java/app/view/FileMenuBar.java
@@ -10,17 +10,17 @@ public class FileMenuBar extends MenuBar
     public FileMenuBar()
     {
         // File menu
-        Menu fileMenu = new Menu("_File");
+        Menu fileMenu = new Menu("File");
         // File items
-        MenuItem newFile = new MenuItem("_New");
-        MenuItem openFile = new MenuItem("_Open");
+        MenuItem newFile = new MenuItem("New");
+        MenuItem openFile = new MenuItem("Open");
         newFile.setOnAction(e -> System.out.println("Test new..."));
         openFile.setOnAction(e -> System.out.println("Test open..."));
         fileMenu.getItems().add(newFile);
         fileMenu.getItems().add(openFile);
 
         // Map Builder menu
-        Menu mapBuildMenu = new Menu("_Map Builder");
+        Menu mapBuildMenu = new Menu("Map Builder");
         // Map Builder item
         MenuItem mapBuilder = new MenuItem("Map Builder");
         mapBuilder.setOnAction(e -> System.out.println("Test map builder"));
