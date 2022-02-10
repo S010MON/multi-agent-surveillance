118c6c5b2d5a21aaf697014658307df1fe2b0a0e
Jolijn2020 <jolijnbruinen@gmail.com>
2022-02-10 14:51:09 +0100

    created class

---
+++ src/main/java/app/controller/FieldOfView.java
@@ -0,0 +1,16 @@
+package app.controller;
+
+import app.controller.graphicsEngine.Ray;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class FieldOfView {
+
+    public ArrayList<Ray> limitView(ArrayList<Ray> rays, Vector leftLimit, Vector rightlimit)
+    {
+        //ray.getU() is the origin of the ray
+        return rays;
+    }
+
+}
