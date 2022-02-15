662409fdcf16af6f72115954d47e6d0cc7e343ae
Jolijn2020 <jolijnbruinen@gmail.com>
2022-02-15 15:25:30 +0100

    implemented limitView method

--- src/main/java/app/controller/FieldOfView.java
+++ src/main/java/app/controller/FieldOfView.java
@@ -1,15 +1,56 @@
 package app.controller;
 
 import app.controller.graphicsEngine.Ray;
+import app.controller.linAlg.Vector;
 
 import java.util.ArrayList;
-import java.util.Vector;
 
 public class FieldOfView {
 
-    public ArrayList<Ray> limitView(ArrayList<Ray> rays, Vector leftLimit, Vector rightlimit)
+    /**
+     * computes which rays are between the left and right limit clockwise
+     * @param rays all rays to determine if between limits, all origins have to be the same!
+     * @param leftLimit leftLimit of what the Rays can reach
+     * @param rightlimit rightLimit of what the Rays can reach
+     * @return
+     */
+    public static ArrayList<Ray> limitView(ArrayList<Ray> rays, Vector leftLimit, Vector rightlimit)
     {
-        //ray.getU() is the origin of the ray
+        //if no rays in list, return the empty list
+        if(rays.size()==0)
+            return rays;
+
+        //Calculate the angles of the limits with respect to the origin's of the rays
+        Ray leftRay = new Ray(rays.get(0).getU(), leftLimit);
+        Ray rightRay = new Ray(rays.get(0).getU(), rightlimit);
+        double leftAngle = leftRay.angle();
+        double rightAngle = rightRay.angle();
+
+        //if left and right angle are the same, than everything is between them
+        if(leftAngle==rightAngle)
+            return rays;
+
+
+        ArrayList<Ray> acceptedRays = new ArrayList<>();
+        // if rightAngle > leftAngle, return all rays with angle between them
+        if(rightAngle>leftAngle)
+        {
+            for(Ray ray: rays)
+            {
+                double rayAngle = ray.angle();
+                if(rayAngle>leftAngle && rayAngle<rightAngle)
+                    acceptedRays.add(ray);
+            }
+        }
+        else
+        {
+            for(Ray ray: rays)
+            {
+                double rayAngle = ray.angle();
+                if(rayAngle>leftAngle || rayAngle<rightAngle)
+                    acceptedRays.add(ray);
+            }
+        }
 
         return rays;
     }
---
+++ src/test/java/jgfx/javagradlefx/FieldOfViewTest.java
@@ -0,0 +1,13 @@
+package jgfx.javagradlefx;
+
+import org.junit.jupiter.api.Test;
+
+public class FieldOfViewTest {
+
+    @Test
+    void leftSameAsRightLimit()
+    {
+
+    }
+
+}
