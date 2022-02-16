9e7e97112b9e6f88fd959df40efb2536c25da961
Travis <travisd1711@gmail.com>
2022-02-16 13:49:45 +0100

    "Initial construction for Aco Vision to Action class"

--- src/main/java/app/model/agents/ACO/AcoAgent.java
+++ src/main/java/app/model/agents/ACO/AcoAgent.java
@@ -3,10 +3,13 @@ package app.model.agents.ACO;
 import app.controller.linAlg.Vector;
 import app.model.agents.AgentImp;
 
+import java.util.HashMap;
+
 public class AcoAgent extends AgentImp
 {
     //TODO Place actual max pheramone value
     private double maxPheramone = 10;
+    private HashMap<Integer, Cell> agentMap = new HashMap<>();
 
     public AcoAgent(Vector position, Vector direction, double radius)
     {
