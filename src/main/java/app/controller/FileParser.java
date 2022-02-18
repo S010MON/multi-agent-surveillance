31a57e6818e30ed1cedf96e136cc85f2ed272c1a
S010MON <leon@bdebnath.com>
2022-02-18 15:29:20 +0100

    refactor parseNextLine() to not depend on global static variable

--- src/main/java/app/controller/FileParser.java
+++ src/main/java/app/controller/FileParser.java
@@ -8,18 +8,16 @@ import java.util.Scanner;
 
 public class FileParser
 {
-    private static Settings map;
-
     public static Settings readGameFile(String path)
     {
         Path file = Paths.get(path);
-        map = new Settings();
+        Settings settings = new Settings();
         try(Scanner scan = new Scanner(file))
         {
             int countLines=1;
             while(scan.hasNextLine())
             {
-                parseNextLine(scan.nextLine(), countLines);
+                parseNextLine(settings, scan.nextLine(), countLines);
                 countLines++;
             }
         }
@@ -28,11 +26,11 @@ public class FileParser
             e.printStackTrace();
             System.out.println("Failed creating scanner object using path supplied.");
         }
-        map.lock(); // Settings object now immutable
-        return map;
+        settings.lock(); // Settings object now immutable
+        return settings;
     }
 
-    public static void parseNextLine(String nextLine, int countLines)
+    public static void parseNextLine(Settings settings, String nextLine, int countLines)
     {
         try(Scanner scan = new Scanner(nextLine))
         {
@@ -46,34 +44,34 @@ public class FileParser
                 String[] coords = val.split(" ");
                 switch(id)
                 {
-                    case "name" -> map.setName((String) val);
-                    case "gameMode" -> map.setGamemode(Integer.parseInt(val));
-                    case "height" -> map.setHeight(Integer.parseInt(val));
-                    case "width" -> map.setWidth(Integer.parseInt(val));
-                    case "numGuards" -> map.setNoOfGuards(Integer.parseInt(val));
-                    case "numIntruders" -> map.setNoOfIntruders(Integer.parseInt(val));
-                    case "baseSpeedGuard" -> map.setWalkSpeedGuard(Double.parseDouble(val));
-                    case "sprintSpeedGuard" -> map.setSprintSpeedGuard(Double.parseDouble(val));
-                    case "baseSpeedIntruder" -> map.setWalkSpeedIntruder(Double.parseDouble(val));
-                    case "sprintSpeedIntruder" -> map.setSprintSpeedIntruder(Double.parseDouble(val));
-                    case "timeStep" ->  map.setTimeStep(Double.parseDouble(val));
-                    case "scaling" -> map.setScaling(Double.parseDouble(val));
-                    case "wall" -> map.addWall(rectangleOf(coords));
-                    case "shaded" -> map.addShade(rectangleOf(coords));
-                    case "glass" ->  map.addGlass(rectangleOf(coords));
-                    case "tower" ->  map.addTower(rectangleOf(coords));
-                    case "targetArea" -> map.setTargetArea(rectangleOf(coords));
-                    case "spawnAreaIntruders" ->  map.setSpawnAreaIntruders(rectangleOf(coords));
-                    case "spawnAreaGuards" -> map.setSpawnAreaGuards(rectangleOf(coords));
+                    case "name" -> settings.setName(val);
+                    case "gameMode" -> settings.setGamemode(Integer.parseInt(val));
+                    case "height" -> settings.setHeight(Integer.parseInt(val));
+                    case "width" -> settings.setWidth(Integer.parseInt(val));
+                    case "numGuards" -> settings.setNoOfGuards(Integer.parseInt(val));
+                    case "numIntruders" -> settings.setNoOfIntruders(Integer.parseInt(val));
+                    case "baseSpeedGuard" -> settings.setWalkSpeedGuard(Double.parseDouble(val));
+                    case "sprintSpeedGuard" -> settings.setSprintSpeedGuard(Double.parseDouble(val));
+                    case "baseSpeedIntruder" -> settings.setWalkSpeedIntruder(Double.parseDouble(val));
+                    case "sprintSpeedIntruder" -> settings.setSprintSpeedIntruder(Double.parseDouble(val));
+                    case "timeStep" ->  settings.setTimeStep(Double.parseDouble(val));
+                    case "scaling" -> settings.setScaling(Double.parseDouble(val));
+                    case "wall" -> settings.addWall(rectangleOf(coords));
+                    case "shaded" -> settings.addShade(rectangleOf(coords));
+                    case "glass" ->  settings.addGlass(rectangleOf(coords));
+                    case "tower" ->  settings.addTower(rectangleOf(coords));
+                    case "targetArea" -> settings.setTargetArea(rectangleOf(coords));
+                    case "spawnAreaIntruders" ->  settings.setSpawnAreaIntruders(rectangleOf(coords));
+                    case "spawnAreaGuards" -> settings.setSpawnAreaGuards(rectangleOf(coords));
                     case "texture" -> {
-                            map.addTexture(rectangleOf(coords));
-                            map.addTextureType(Integer.parseInt(coords[4]));
-                            map.addTextureOrientation(Integer.parseInt(coords[5])); }
+                            settings.addTexture(rectangleOf(coords));
+                            settings.addTextureType(Integer.parseInt(coords[4]));
+                            settings.addTextureOrientation(Integer.parseInt(coords[5])); }
                     case "teleport" -> {
-                        map.addPortal(rectangleOf(coords));
-                        map.addTeleportTo(new Point(Integer.parseInt(coords[4]),
+                        settings.addPortal(rectangleOf(coords));
+                        settings.addTeleportTo(new Point(Integer.parseInt(coords[4]),
                                                     Integer.parseInt(coords[5])));
-                        map.addTeleportOrientation(Double.parseDouble(coords[6]));}
+                        settings.addTeleportOrientation(Double.parseDouble(coords[6]));}
                 }
             }
         }
