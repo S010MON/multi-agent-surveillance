ec46f34d0c6d7c14caabf8895492710f91ea4a0b
gwentinho <qschroed@gmail.com>
2022-02-21 21:19:29 +0100

    Implemented first version of new ray tracing for sound, right now we just check the highest sound level that can be heard from an agent, but will soon be changed to include obstacles

---
+++ src/main/java/app/controller/soundEngine/CornerAimedTracing.java
@@ -0,0 +1,36 @@
+package app.controller.soundEngine;
+
+import app.controller.linAlg.Vector;
+import app.model.Map;
+import app.model.agents.Agent;
+
+import java.util.ArrayList;
+
+public class CornerAimedTracing implements SoundEngine{
+    public double compute(Map map, Agent agent){
+        ArrayList<SoundRay> soundRays = new ArrayList<>();
+
+        // Step 1: aim at all the soundsources
+        // right now this means ignore obstactles and return the loudest audible sound from agent
+
+        return map.getSoundSources().stream().map(source -> source.soundLevelFrom(agent.getPosition())).max(Double::compare).get();
+
+        /*
+        for (SoundSource soundSource: map.getSoundSources()){
+            soundRays.add(new SoundRay(agent.getPosition(), soundSource.getPosition()));
+        }
+         */
+
+
+        // Step 2: for diffraction aim at all the corners
+        /*
+        for (SoundFurniture soundFurniture: map.getSoundFurniture()) {
+            for (SoundBoundary soundBoundary: soundFurniture.getSoundBoundaries()) {
+                for (Vector corner: soundBoundary.getCorners()) {
+                    soundRays.add(new SoundRay(agent.getPosition(), corner));
+                }
+            }
+        }
+        */
+    }
+}
