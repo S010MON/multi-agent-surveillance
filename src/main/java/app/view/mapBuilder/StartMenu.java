acf2221d81f42a8e1e8979bc73ecf388baf0ea61
MatthewFoster02 <mattfoster02@outlook.com>
2022-02-24 12:53:50 +0100

    Save settings
    Gathers the settings from both furniture and setting panes.

--- src/main/java/app/view/mapBuilder/StartMenu.java
+++ src/main/java/app/view/mapBuilder/StartMenu.java
@@ -1,7 +1,9 @@
 package app.view.mapBuilder;
 
 import app.App;
+import app.controller.settings.Settings;
 import app.controller.settings.SettingsGenerator;
+import app.controller.settings.SettingsObject;
 import app.view.FileMenuBar;
 import javafx.scene.layout.BorderPane;
 import lombok.Getter;
@@ -9,14 +11,43 @@ import lombok.Getter;
 public class StartMenu extends BorderPane
 {
     @Getter private App app;
+    private MapBuilder mb;
+    private FurniturePane fp;
+    private SettingsPane sp;
+    private Settings settings;
 
     public StartMenu(App app)
     {
         this.app = app;
         this.setTop(new FileMenuBar(app));
-        MapBuilder mb = new MapBuilder(this);
-        this.setLeft(new FurniturePane(this, mb));
-        this.setRight(new SettingsPane(this, SettingsGenerator.mockSettings()));
+        settings = SettingsGenerator.mockSettings();
+        mb = new MapBuilder(this);
+        fp = new FurniturePane(this, mb);
+        sp = new SettingsPane(this, settings);
+        this.setLeft(fp);
+        this.setRight(sp);
         this.setCenter(mb);
+    }
+
+    public void saveSettings()
+    {
+        fp.getSettings(settings);
+        sp.getSettings();
+
+        // Test
+        System.out.println(settings.getName());
+        System.out.println(settings.getGameMode());
+        System.out.println(settings.getTimeStep());
+        System.out.println(settings.getScaling());
+        System.out.println(settings.getNoOfGuards());
+        System.out.println(settings.getNoOfIntruders());
+        System.out.println(settings.getWalkSpeedGuard());
+        System.out.println(settings.getWalkSpeedIntruder());
+        System.out.println(settings.getSprintSpeedGuard());
+        System.out.println(settings.getSprintSpeedGuard());
+        for(SettingsObject so : settings.getFurniture())
+        {
+            System.out.println(so.toString());
+        }
     }
 }
