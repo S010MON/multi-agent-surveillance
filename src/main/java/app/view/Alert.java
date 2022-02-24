b167f6cb78d35cd1ab63a88faca0097114feed08
MatthewFoster02 <mattfoster02@outlook.com>
2022-02-25 00:52:09 +0100

    Added Alert class
    Alert class which takes in window title and message to display to user.
    
    Map Name on settings pane is effectively the file name so no need to ask the user for this but could still be added if it is a must.

---
+++ src/main/java/app/view/Alert.java
@@ -0,0 +1,37 @@
+package app.view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Alert {
+
+    public static void displayAlert(String title, String message){
+        Stage window=new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(500);
+
+        Label lbl=new Label();
+        lbl.setText(message);
+        Button closeButton=new Button("OK");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout=new VBox(10);
+        layout.getChildren().addAll(lbl, closeButton);
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(15, 15, 15, 15));
+
+        Scene sc=new Scene(layout);
+        window.setScene(sc);
+        window.showAndWait();
+    }
+
+    // TODO could add a method where a text field is also supplied for the user to interact with
+}
