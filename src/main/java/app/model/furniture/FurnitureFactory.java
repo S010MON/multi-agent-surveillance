4874745b3331599a76669704f9d82aca060164ae
S010MON <leon@bdebnath.com>
2022-03-07 16:44:24 +0100

    portals teleport to added

--- src/main/java/app/model/furniture/FurnitureFactory.java
+++ src/main/java/app/model/furniture/FurnitureFactory.java
@@ -1,16 +1,17 @@
 package app.model.furniture;
 
+import app.controller.settings.SettingsObject;
 import app.model.boundary.BoundaryFactory;
 import app.model.texture.TextureFactory;
 import javafx.geometry.Rectangle2D;
 
 public abstract class FurnitureFactory
 {
-    public static Furniture make(FurnitureType type, Rectangle2D rectangle)
+    public static Furniture make(SettingsObject object)
     {
         Furniture furniture = new FurnitureBase();
-        furniture.addBoundaries(BoundaryFactory.make(type, rectangle));
-        furniture.setTexture(TextureFactory.make(type, rectangle));
+        furniture.addBoundaries(BoundaryFactory.make(object));
+        furniture.setTexture(TextureFactory.make(object.getType(), object.getRect()));
         return furniture;
     }
 }
