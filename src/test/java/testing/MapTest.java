19fe112208b4bef81ee997a7cae3ff6c00f3ac31
S010MON <leon@bdebnath.com>
2022-03-11 21:35:13 +0100

    patches shitty map test

--- src/test/java/testing/MapTest.java
+++ src/test/java/testing/MapTest.java
@@ -10,12 +10,12 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MapTest {
-
+public class MapTest
+{
     @Test
     void setSpeedsAgentsInConstructionWithSettings()
     {
-        Settings s = FileManager.loadSettings("src/test/resources/mytest.txt");
+        Settings s = FileManager.loadSettings("src/test/resources/speedAgentTestMap");
         Map map = new Map(s);
         ArrayList<Agent> agents = map.getAgents();
         double maxWalk = s.getWalkSpeedGuard();
