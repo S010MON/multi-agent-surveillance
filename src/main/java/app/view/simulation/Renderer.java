46ef0deadb74e713a8a30b8944c9e1d45ce959b8
S010MON <leon@bdebnath.com>
2022-03-17 13:17:03 +0100

    adds click to select agent

--- src/main/java/app/view/simulation/Renderer.java
+++ src/main/java/app/view/simulation/Renderer.java
@@ -1,8 +1,10 @@
 package app.view.simulation;
 
 import app.controller.graphicsEngine.Ray;
+import app.controller.linAlg.Vector;
 import app.model.Map;
 import app.model.Trail;
+import app.model.agents.Agent;
 import app.view.ScreenSize;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
@@ -10,7 +12,6 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
-
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
@@ -95,13 +96,16 @@ public class Renderer extends Canvas
     private void mousePressed(MouseEvent e)
     {
         click = new Point2D(e.getX(), e.getY());
-        System.out.println("click = " + click);
+
+        Vector vector = convertToMapVector(click);
+        Agent agent = selectAgent(vector);
+        if(agent != null)
+            System.out.println(agent);
     }
 
     private void mouseReleased(MouseEvent e)
     {
         click = null;
-        System.out.println("click = null");
     }
 
     private void mouseDragged(MouseEvent e)
@@ -126,5 +130,27 @@ public class Renderer extends Canvas
             Info.getInfo().setZoom(Math.max(dy,dx)-padding);
         else
             Info.getInfo().setZoom(Math.min(dy,dx)-padding);
+    }
+
+    private Agent selectAgent(Vector v)
+    {
+        for(Agent a: map.getAgents())
+        {
+            if(v.dist(a.getPosition()) <= 20)
+                return a;
+        }
+        return null;
+    }
+
+    private Vector convertToMapVector(Point2D point)
+    {
+        return new Vector((point.getX() - Info.getInfo().offsetX)/Info.getInfo().getZoom(),
+                          (point.getY() - Info.getInfo().offsetY)/Info.getInfo().getZoom());
+    }
+
+    private Point2D convertToScreenPoint(Vector vector)
+    {
+        return new Point2D(vector.getX() * Info.getInfo().getZoom() + Info.getInfo().offsetX,
+                           vector.getY() * Info.getInfo().getZoom() + Info.getInfo().offsetY);
     }
 }
