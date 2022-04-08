af1c74939904870faf0f03b0fefb256f1e00cd21
S010MON <leon@bdebnath.com>
2022-04-08 14:30:25 +0200

    added boundary intersection to sound engine

--- src/main/java/app/controller/soundEngine/SoundEngine.java
+++ src/main/java/app/controller/soundEngine/SoundEngine.java
@@ -1,23 +1,40 @@
 package app.controller.soundEngine;
 
+import app.controller.graphicsEngine.Ray;
 import app.controller.linAlg.Vector;
 import app.model.Map;
 import app.model.agents.Agent;
+import app.model.boundary.Boundary;
 import app.model.soundBoundary.SoundBoundary;
 import java.util.ArrayList;
 
 public class SoundEngine
 {
-    public ArrayList<SoundRay> compute(Map map, Agent agent)
+    public ArrayList<SoundRay> compute(Map map, Vector soundSource)
     {
         ArrayList<SoundBoundary> boundaries = collectSoundBoundaries(map);
         ArrayList<SoundRay> output = new ArrayList<>();
-        ArrayList<SoundRay> rays = SoundRayScatter.angle360(agent.getPosition(), 10, 5);
-        Vector origin = agent.getPosition();
+        ArrayList<SoundRay> rays = SoundRayScatter.angle360(soundSource, 10, 5);
+        Vector origin = soundSource;
 
         for(SoundRay r: rays)
         {
-            Vector agentIntersection = getIntersection(r, map.getAgents(), agent);
+            Vector agentIntersection = getIntersection(r, map.getAgents(), null);
+            Vector bdyIntersection = getIntersection(r, boundaries);
+
+            if(bdyIntersection != null && agentIntersection != null)
+            {
+                if(bdyIntersection.dist(origin) <= agentIntersection.dist(origin))
+                {
+                    output.add(new SoundRay(origin, bdyIntersection));
+                }
+                else
+                {
+                    output.add(new SoundRay(origin, agentIntersection));
+                }
+            }
+            else if(bdyIntersection != null)
+                output.add(new SoundRay(origin, bdyIntersection));
 
             if(agentIntersection != null)
                 output.add(new SoundRay(origin, agentIntersection));
@@ -25,23 +42,40 @@ public class SoundEngine
         return output;
     }
 
-    private Vector getIntersection(SoundRay r, ArrayList<Agent> agents, Agent currentAgent)
+    private Vector getIntersection(SoundRay r, ArrayList<SoundBoundary> boundaries)
+    {
+        Vector intersection = null;
+        double closestDist = Double.MAX_VALUE;
+        for (SoundBoundary obj: boundaries)
+        {
+            if(obj.intersects(r))
+            {
+                Vector currentV = obj.intersection(r);
+                double dist = r.getU().dist(currentV);
+                if(dist < closestDist)
+                {
+                    intersection = currentV;
+                    closestDist = dist;
+                }
+            }
+        }
+        return intersection;
+    }
+
+    private Vector getIntersection(SoundRay r, ArrayList<Agent> agents, Agent leaveNull)
     {
         Vector intersection = null;
         double closestDist = Double.MAX_VALUE;
         for (Agent agent: agents)
         {
-            if(!agent.equals(currentAgent))
+            if (agent.isHit(r))
             {
-                if (agent.isHit(r))
+                Vector currentV = agent.intersection(r);
+                double dist = r.getU().dist(currentV);
+                if (dist < closestDist && currentV.getAngle() == r.angle())
                 {
-                    Vector currentV = agent.intersection(r);
-                    double dist = r.getU().dist(currentV);
-                    if (dist < closestDist && currentV.getAngle() == r.angle())
-                    {
-                        intersection = currentV;
-                        closestDist = dist;
-                    }
+                    intersection = currentV;
+                    closestDist = dist;
                 }
             }
         }
