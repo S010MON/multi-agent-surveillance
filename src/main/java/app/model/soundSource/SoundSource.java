c4dca678886b8f74d8d0430870032ea9f4f87c9f
S010MON <leon@bdebnath.com>
2022-04-08 13:41:37 +0200

    refactor for style

--- src/main/java/app/model/soundSource/SoundSource.java
+++ src/main/java/app/model/soundSource/SoundSource.java
@@ -3,7 +3,9 @@ package app.model.soundSource;
 
 import app.controller.linAlg.Vector;
 
-public interface SoundSource {
+public interface SoundSource
+{
     double soundLevelFrom(Vector position, int diffractionCount);
+
     Vector getPosition();
 }
