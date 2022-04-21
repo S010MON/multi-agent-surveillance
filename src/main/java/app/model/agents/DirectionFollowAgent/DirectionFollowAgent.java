bfd1fb75508f3091168f696eb4b57cdad585cab0
Jolijn2020 <jolijnbruinen@gmail.com>
2022-04-21 12:24:52 +0200

    added noWallDetected method

--- src/main/java/app/model/agents/DirectionFollowAgent/DirectionFollowAgent.java
+++ src/main/java/app/model/agents/DirectionFollowAgent/DirectionFollowAgent.java
@@ -5,6 +5,8 @@ import app.controller.linAlg.Vector;
 import app.model.Move;
 import app.model.agents.AgentImp;
 import app.model.agents.Team;
+import lombok.Getter;
+import lombok.Setter;
 
 
 public class DirectionFollowAgent extends AgentImp
@@ -18,6 +20,8 @@ public class DirectionFollowAgent extends AgentImp
 
     private Ray targetRay;
     private InternalState internalState;
+    private boolean DEBUG = false;
+    @Getter @Setter private double moveLength;
 
     public DirectionFollowAgent(Vector position, Vector direction, double radius, Team team, Vector targetDirection)
     {
@@ -30,6 +34,17 @@ public class DirectionFollowAgent extends AgentImp
         internalState = InternalState.followRay;
     }
 
+    public DirectionFollowAgent(Vector position, Vector direction, double radius, Team team, Vector targetDirection, double moveLen)
+    {
+        super(position, direction, radius, team);
+        if(targetDirection == null)
+        {
+            throw new RuntimeException("no targetDirection provided for intruder");
+        }
+        targetRay = new Ray(position, targetDirection);
+        internalState = InternalState.followRay;
+        this.moveLength = moveLen;
+    }
 
 
     @Override
@@ -59,4 +74,29 @@ public class DirectionFollowAgent extends AgentImp
     {
         return null;
     }
+
+    /**
+     * Method for checking for walls/obstacles for getting next move in the wall following algorithm.
+     * Walls/obstacles are checked in the direction of the given rayAngle by checking if that ray detects
+     * an obstacle within the moveLength distance range.
+     * @param rayAngle angle of the direction to be checked.
+     * @return true if no obstacle detected; false if obstacle detected
+     */
+    private boolean noWallDetected(double rayAngle)
+    {
+        for (Ray r : view)
+        {
+            if ((r.angle() <= rayAngle + 1.0 && r.angle() >= rayAngle - 1.0) && r.rayLength() <= moveLength)
+            {
+                if (DEBUG)
+                    System.out.println("WALL DETECTED! Ray Angle: " + rayAngle);
+                return false;
+            }
+        }
+        if (DEBUG)
+            System.out.println("No wall detected with ray of angle: " + rayAngle);
+        return true;
+    }
+
+
 }
