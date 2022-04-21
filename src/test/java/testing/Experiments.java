2f246361af9a5a3ecec5dfe17e71742f99f1f885
Travis <travisd1711@gmail.com>
2022-04-21 13:18:25 +0200

    "Experiment file adjusted, should contain all information from current master"

--- src/test/java/testing/Experiments.java
+++ src/test/java/testing/Experiments.java
@@ -9,7 +9,7 @@ import app.model.agents.AgentType;
 
 public class Experiments
 {
-    private static final int iterations = 100;
+    private static final int iterations = 2;
     private static final int[] no_of_agents = {1, 2, 3, 4 ,5};
     private static final AgentType[] agents = {AgentType.ACO, AgentType.WALL_FOLLOW, AgentType.RANDOM};
 
@@ -18,7 +18,7 @@ public class Experiments
      */
     public static void main(String[] args)
     {
-        //run("experiment_map_1");
+        run("experiment_map_1");
     }
 
     private static void run(String map_name)
@@ -50,38 +50,6 @@ public class Experiments
                     int[] data = gameEngine.run();
                     logger.log(agent_heading + test_name, data);
                 }
-            }
-        }
-    }
-    public static void main(String[] args)
-    {
-        AgentType agent_under_test = AgentType.ACO;
-        int no_of_agents = 1;
-        int iterations = 100;
-        int map_start_range = 1;
-        int map_end_range = 5;
-
-        for(int n = map_start_range; n < map_end_range; n++)
-        {
-            String map_path = "src/main/resources/";
-            String map_name = "experiment_map_" + n;
-            System.out.println("Loading map: " + map_name);
-            Settings settings = FileManager.loadSettings(map_path + map_name);
-
-            System.out.println("Agent Type: " + agent_under_test);
-            settings.setGuardType(agent_under_test);
-            settings.setNoOfGuards(no_of_agents);
-            settings.setNoOfIntruders(0);
-
-            Logger logger = new Logger(map_name);
-            logger.setOutputCsv();
-
-            for(int i = 0; i < iterations; i++)
-            {
-                Map map = new Map((settings));
-                TestingEngine gameEngine = new TestingEngine(map);
-                int[] data = gameEngine.run();
-                logger.log(agent_under_test.name(), data);
             }
         }
     }
