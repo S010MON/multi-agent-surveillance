2339a06bed6f9022c28fd557b69fa66a4a506e37
Jolijn2020 <jolijnbruinen@gmail.com>
2022-04-21 13:03:43 +0200

    worked on wallfollowAlgorithm in DirectionFollowAgent

--- src/main/java/app/model/agents/DirectionFollowAgent/DirectionFollowAgent.java
+++ src/main/java/app/model/agents/DirectionFollowAgent/DirectionFollowAgent.java
@@ -4,7 +4,9 @@ import app.controller.graphicsEngine.Ray;
 import app.controller.linAlg.Vector;
 import app.model.Move;
 import app.model.agents.AgentImp;
+import app.model.agents.Cells.BooleanCell;
 import app.model.agents.Team;
+import app.model.agents.WallFollow.WallFollowAgent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +20,19 @@ public class DirectionFollowAgent extends AgentImp
         goToTarget,
     }
 
+    public enum TurnType
+    {
+        LEFT,
+        RIGHT,
+        NO_TURN,
+    }
+
     private Ray targetRay;
     private InternalState internalState;
     private boolean DEBUG = false;
-    @Getter @Setter private double moveLength;
+    @Getter @Setter private double moveLength = 20;
+    private TurnType lastTurn;
+    private TurnType wallTurn;
 
     public DirectionFollowAgent(Vector position, Vector direction, double radius, Team team, Vector targetDirection)
     {
@@ -31,7 +42,9 @@ public class DirectionFollowAgent extends AgentImp
             throw new RuntimeException("no targetDirection provided for intruder");
         }
         targetRay = new Ray(position, targetDirection);
+
         internalState = InternalState.followRay;
+        lastTurn = TurnType.NO_TURN;
     }
 
     public DirectionFollowAgent(Vector position, Vector direction, double radius, Team team, Vector targetDirection, double moveLen)
@@ -42,8 +55,10 @@ public class DirectionFollowAgent extends AgentImp
             throw new RuntimeException("no targetDirection provided for intruder");
         }
         targetRay = new Ray(position, targetDirection);
+        moveLength = moveLen;
+
         internalState = InternalState.followRay;
-        this.moveLength = moveLen;
+        lastTurn = TurnType.NO_TURN;
     }
 
 
@@ -98,5 +113,66 @@ public class DirectionFollowAgent extends AgentImp
         return true;
     }
 
+    /** Pseudocode for simple wall following algorithm:
+         if (turned wallTurn previously and forward no wall)
+            go forward;
+         else if (no wall at wallTurn)
+            turn 90 deg wallTurn;
+         else if (no wall forward)
+            go forward;
+         else
+            turn 90 deg opposite of wallTurn;
 
+     * based on: https://blogs.ntu.edu.sg/scemdp-201718s1-g14/exploration-algorithm/
+     */
+
+    public Move runWallFollowAlgorithm()
+    {
+        Vector newMove = new Vector(0,0);
+        Vector newDirection = direction;
+        if (lastTurn == wallTurn && noWallDetected(direction.getAngle()))
+        {
+            if (DEBUG) { System.out.println("ALGORITHM CASE 1"); }
+            newMove = new Vector(moveLength * direction.getX(), moveLength * direction.getY());
+            lastTurn = TurnType.NO_TURN;
+        }
+        else if (noWallDetected(getAngleOfLeftRay()) && !leftCell.getObstacle())
+        {
+            if (DEBUG) { System.out.println("Angle of left ray: " + getAngleOfLeftRay()); ; }
+            if (DEBUG) { System.out.println("ALGORITHM CASE 2"); }
+            // TODO: make rotateAgent(TurnType turn) method and use here
+            newDirection = rotateAgentLeft();
+            lastTurn = WallFollowAgent.TurnType.LEFT;
+            movedForwardLast = false;
+        }
+        else if (noWallDetected(direction.getAngle()) && !forwardCell.getObstacle())
+        {
+            if (DEBUG) { System.out.println("ALGORITHM CASE 3"); }
+            newMove = new Vector(moveLength * direction.getX(), moveLength * direction.getY());
+            movedForwardLast = true;
+            lastTurn = WallFollowAgent.TurnType.NO_TURN;
+            markWallAsCovered();
+        }
+        else
+        {
+            if (DEBUG) { System.out.println("ALGORITHM CASE 4"); }
+            newDirection = rotateAgentRight();
+            lastTurn = WallFollowAgent.TurnType.RIGHT;
+            movedForwardLast = false;
+        }
+        return new Move(newDirection,newMove);
+    }
+
+    public double getAngleOfLeftRay()
+    {
+        double currentAngle = direction.getAngle();
+        if (currentAngle < 270)
+        {
+            return currentAngle + 90;
+        }
+        else
+        {
+            return currentAngle + 90 - 360;
+        }
+    }
 }
