5e7ace79c5dc5ce686546f77dbc32caa3ba9c76f
Jolijn2020 <jolijnbruinen@gmail.com>
2022-05-16 16:36:19 +0200

    fixed new usage of vision in AcoAgent

--- src/main/java/app/model/agents/ACO/AcoAgent.java
+++ src/main/java/app/model/agents/ACO/AcoAgent.java
@@ -320,7 +320,7 @@ public class AcoAgent extends AgentImp
     {
         for(Ray ray: view)
         {
-            if(TypeInformation.isSolid(ray.getType()) && approximateAngleRange(ray.angle(), targetCardinalAngle))
+            if((TypeInformation.isSolid(ray.getType()) || ray.getType() ==null) && approximateAngleRange(ray.angle(), targetCardinalAngle))
             {
                 return ray;
             }
