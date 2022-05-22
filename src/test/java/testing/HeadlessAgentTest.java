e6fa13a69b97aa225c46a046bcc79cfed0427468
Jolijn2020 <jolijnbruinen@gmail.com>
2022-05-22 11:54:29 +0200

    disabled runCoverageTest, it doesn't test anything and is actually experiment

--- src/test/java/testing/HeadlessAgentTest.java
+++ src/test/java/testing/HeadlessAgentTest.java
@@ -6,6 +6,7 @@ import app.controller.logging.Logger;
 import app.controller.settings.Settings;
 import app.model.Map;
 import app.model.agents.AgentType;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class HeadlessAgentTest
@@ -32,6 +33,9 @@ public class HeadlessAgentTest
         logger.log(typeIntruder + " " + typeGuard + " test", data);
     }
 
+    // This doesn't even test anything?
+    // and it takes way too long, 100000 tics is almost 3 hours worth of time (10 tics = 1 sec)
+    @Disabled
     @Test public void ACOWFTest()
     {
         // intruder ACO, guard WF
