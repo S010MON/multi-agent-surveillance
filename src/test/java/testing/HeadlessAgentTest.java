d815ffe888197c7aa4959baf045b272902b6e215
Jolijn2020 <jolijnbruinen@gmail.com>
2022-05-22 13:22:04 +0200

    disabled Coverage experiment
    accidently enabled it in another commit

--- src/test/java/testing/HeadlessAgentTest.java
+++ src/test/java/testing/HeadlessAgentTest.java
@@ -35,10 +35,10 @@ public class HeadlessAgentTest
 
     // This doesn't even test anything?
     // and it takes way too long, 100000 tics is almost 3 hours worth of time (10 tics = 1 sec)
-    //@Disabled
+    @Disabled
     @Test public void ACOWFTest()
     {
         // intruder ACO, guard WF
-        runOnce("experiment_map_1", AgentType.ACO, AgentType.WALL_FOLLOW);
+        runOnce("experiment_map_1", AgentType.WALL_FOLLOW, AgentType.ACO);
     }
 }
